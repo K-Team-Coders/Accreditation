6632bd27b1a06fd51f41a241180abeaa190f24ab
Shisha <shishkarev16@gmail.com>
2023-10-28 23:43:30 +0300

    Update TableSite.vue

--- frontend/src/components/TableSite.vue
+++ frontend/src/components/TableSite.vue
@@ -4,38 +4,38 @@
       Выберите строчку для детального просмотра списков оборудований
     </p>
     <div
-      class="shadow-md sm:rounded-lg rounded-lg overflow-x-auto h-[500px] xl:max-w-7xl lg:max-w-4xl md:max-w-3xl sm:max-w-2xl mx-auto max-w-[300px]"
+      class="shadow-md sm:rounded-lg rounded-lg overflow-auto h-[500px] xl:max-w-[1800px] lg:max-w-4xl md:max-w-3xl sm:max-w-2xl mx-auto max-w-[300px]"
     >
-      <table class="w-full text-xs text-gray-500 table-auto text-center">
+      <table class="w-full text-xs text-gray-500 table-auto xl:table-fixed text-center">
         <thead class="text-xs text-gray-700 uppercase bg-gray-50 font-bold">
-          <tr>
+          <tr class="">
             <th
               scope="col"
-              class="px-6 py-3 cursor-pointer hover:text-[#e40046]"
+              class="px-1 py-3 xl:w-10 cursor-pointer hover:text-[#e40046]"
             >
               ID
             </th>
             <th
               scope="col"
-              class="px-6 py-3 cursor-pointer hover:text-[#e40046]"
+              class="px-6 py-3 xl:w-72 cursor-pointer hover:text-[#e40046]"
             >
               Обозначение стандарта
             </th>
             <th
               scope="col"
-              class="px-6 py-3 cursor-pointer hover:text-[#e40046]"
+              class="px-6 py-3 xl:w-72 cursor-pointer hover:text-[#e40046]"
             >
               Группа продукции
             </th>
             <th
               scope="col"
-              class="px-6 py-3 cursor-pointer hover:text-[#e40046]"
+              class="px-6 py-3 xl:w-72 cursor-pointer hover:text-[#e40046]"
             >
               Наименование продукции
             </th>
             <th
               scope="col"
-              class="px-6 py-3 cursor-pointer hover:text-[#e40046]"
+              class="px-6 py-3 xl:w-35 cursor-pointer hover:text-[#e40046]"
             >
               Коды ОКПД 2 / ТН ВЭД ЕАЭС
             </th>
@@ -53,13 +53,13 @@
             </th>
             <th
               scope="col"
-              class="px-6 py-3 cursor-pointer hover:text-[#e40046]"
+              class="px-6 py-3  cursor-pointer hover:text-[#e40046]"
             >
               % соответствия
             </th>
           </tr>
         </thead>
-        <tbody class="font-semibold ">
+        <tbody class="font-semibold">
           <tr
             @click="isModalOpen = true"
             v-for="el in 7"
@@ -67,17 +67,43 @@
           >
             <th
               scope="row"
-              class="px-6 py-4 max-w-lg font-medium text-gray-900 whitespace-nowrap truncate"
+              class="px-2 py-2 max-w-lg font-medium text-gray-900 whitespace-nowrap truncate"
             >
               {{ el }}
             </th>
-            <td class="px-6 py-4">{{ el }}</td>
-            <td class="px-6 py-4">{{ el + 3 }}</td>
-            <td class="px-6 py-4">{{ el + 9 }}</td>
-            <td class="px-6 py-4">{{ el + 1 }}</td>
-            <td class="px-6 py-4">{{ el + 82 }}</td>
-            <td class="px-6 py-4">{{ el + 8 }}</td>
-            <td class="px-6 py-4">{{ el + 28 }}</td>
+            <td class="px-2 py-2">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit
+              natus modi, aliquam placeat, doloribus tenetur voluptatem ex a,
+              officiis tempore dolor doloremque. Exercitationem dolor,
+              dignissimos fugiat optio ipsam ad aliquam.
+            </td>
+            <td class="px-2 py-2">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa
+              esse at provident impedit in quia magnam, aliquam modi tempora,
+              voluptatem corrupti. Et optio sed ratione ipsa quas odio libero
+              sequi?
+            </td>
+            <td class="px-2 py-2">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
+              doloribus similique tenetur eos, odio, repellendus architecto
+              ullam nisi est exercitationem quis repudiandae. Suscipit ex et
+              expedita, vero iusto rerum laboriosam.Lorem ipsum dolor sit amet
+              consectetur adipisicing elit. Impedit doloribus similique tenetur
+              eos, odio, repellendus architecto ullam nisi est exercitationem
+              quis repudiandae. Suscipit ex et expedita, vero iusto rerum
+              laboriosam.
+            </td>
+            <td class="px-2 py-2">127367612376 12376128361283</td>
+            <td class="px-2 py-2">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
+              doloribus similique tenetur eos, odio, repellendus architecto
+              ullam nisi est exercitationem quis repudiandae. Suscipit ex et
+            </td>
+            <td class="px-2 py-2">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
+              doloribus similique tenetur eos, odio, repellendus architecto
+            </td>
+            <td class="px-2 py-2">4123</td>
           </tr>
         </tbody>
       </table>
