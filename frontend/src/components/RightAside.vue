c0b6b2759b354d776f0b55ff770b36c8d45e829e
Your Carma <y0urcarma@yandex.ru>
2023-10-28 14:05:32 +0300

    Update RightAside.vue

--- frontend/src/components/RightAside.vue
+++ frontend/src/components/RightAside.vue
@@ -1,30 +1,34 @@
 <template>
   <div class="py-3">
-    <form class="mb-2">
-      <label
-        for="default-search"
-        class="mb-2 text-sm font-medium text-gray-900 sr-only"
-        >Искать</label
-      >
-      <div class="relative">
+    Обработанные файлы
+    <form>
+      <div class="flex items-center py-2">
         <input
-          type="search"
-          id="default-search"
-          class="block w-full p-3 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-red-500 focus:border-red-500"
-          placeholder="Введите что-нибудь"
-          required
+        v-on:change="handleFilesUpload()"
+          class="w-full text-sm text-gray-700 border-[0.5px] py-1 px-2 border-[#e40046] rounded-lg cursor-pointer bg-gray-50 focus:outline-none"
+          aria-describedby="file_input_help"
+          id="files"
+          ref="files"
+          type="file"
         />
+        <p class="mt-2.5 ml-2 text-sm text-gray-500" id="file_input_help">
+          .docx
+        </p>
+      </div>
+      <div class="flex justify-end pt-2">
         <button
+        @click="submitFiles()" 
           type="submit"
-          class="text-white absolute right-3 bottom-2 bg-[#e40046] hover:bg-red-700 duration-300 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-4 py-2"
+          class="text-whitesmoke bg-[#e40046] hover:bg-[#ce134b] focus:ring-4 focus:outline-none focus:ring-[#ce134b] font-semibold rounded-lg text-sm w-full sm:w-auto px-4 py-2.5 text-center mt-2"
         >
-          Искать
+          Загрузить
         </button>
       </div>
-    </form>
+    </form>    Обработанные руководящие документы
     <div
       class="text-lg h-96 grid grid-cols-3 gap-2 py-4 text-gray-800 font-roboto font-medium text-left overflow-y-auto"
     >
+   
       <div
         v-for="i in 20"
         class="cursor-pointer shadow-md hover:text-[#e40046] text px-4 py-2 duration-300 bg-gray-300 rounded-lg"
