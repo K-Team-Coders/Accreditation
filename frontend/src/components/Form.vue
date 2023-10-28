b3434b4d42fb59ca3279750d0c6d52c9634d996d
Shisha <shishkarev16@gmail.com>
2023-10-28 14:02:00 +0300

    +fix

--- frontend/src/components/Form.vue
+++ frontend/src/components/Form.vue
@@ -1,46 +1,37 @@
 <template>
-  <div>
-  <div
-    class="bg-gray-50 2xl:max-w-4xl xl:max-w-3xl lg:max-w-2xl md:max-w-2xl sm:max-w-xl max-w-[320px] p-4 w-full px-4 py-3 rounded-lg shadow-md"
-  >
-    <form class="">
-      <div class="flex items-center border-b border-[#e40046] py-2">
-        <input
-          v-model="text"
-          @input="text_processing()"
-          class="appearance-none bg-transparent border-none w-full text-gray-700 font-semibold mr-3 py-1 px-2 leading-tight focus:outline-none"
-          type="text"
-          placeholder="Введите наименование продукта"
-          aria-label="Full name"
-        />
-      </div>
-    </form>
-    
+  <div class="flex flex-col justify-center items-center">
+    <div
+      class="bg-gray-50 2xl:max-w-4xl xl:max-w-3xl lg:max-w-2xl md:max-w-2xl sm:max-w-xl max-w-[320px] p-4 w-full px-4 py-3 rounded-lg shadow-md"
+    >
+      <form class="">
+        <div class="flex items-center border-b border-[#e40046] py-2">
+          <input
+            v-model="text"
+            @input="text_processing()"
+            class="appearance-none bg-transparent border-none w-full text-gray-700 font-semibold mr-3 py-1 px-2 leading-tight focus:outline-none"
+            type="text"
+            placeholder="Введите наименование продукта"
+            aria-label="Full name"
+          />
+        </div>
+      </form>
+    </div>
+    <div class="grid grid-cols-2 gap-2 mt-10 mb-4">
+      <Cards name="book" text="12367-123" title="ГОСТ: " />
+      <Cards name="code" text="1746293568487" title="ТН ВЕД: " />
+      <Cards name="tech" text="Текст" title="Оборудование: " />
+      <Cards name="list" text="Текст" title="Группа продукции: " />
+    </div>
+    <div class="flex justify-center pb-10">
+      <DownloadButtonGost />
+    </div>
   </div>
-  <div class="grid grid-cols-2 gap-2 mt-10 mb-4">
-          <Cards name="book" text="12367-123" title="ГОСТ: " />
-          <Cards name="code" text="1746293568487" title="ТН ВЕД: " />
-          <Cards
-            name="tech"
-            text="Текст"
-            title="Оборудование: "
-          />
-          <Cards
-            name="list"
-            text="Текст"
-            title="Группа продукции: "
-          />
-        </div>
-        <div class="flex justify-center pb-10">
-          <DownloadButtonGost />
-        </div>
-      </div>
 </template>
 <script>
 import axios from "axios";
 import debounce from "lodash/debounce";
-import Cards from './Cards.vue';
-import DownloadButtonGost from './DownloadButtonGost.vue';
+import Cards from "./Cards.vue";
+import DownloadButtonGost from "./DownloadButtonGost.vue";
 export default {
   components: { Cards, DownloadButtonGost },
   data() {
@@ -50,13 +41,13 @@ export default {
       textisProcessing: false,
       isError: false,
     };
-},
-methods: {
+  },
+  methods: {
     startTyping() {
       this.isTyping = true;
       this.debounceStopTyping();
     },
-    textChange() { },
+    textChange() {},
     debounceStopTyping: debounce(function () {
       this.isTyping = false;
     }, 500),
@@ -88,6 +79,6 @@ methods: {
         }
       }, 600);
     },
-    }}
-
+  },
+};
 </script>
